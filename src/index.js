11908db695a27e192e9b0cde4fb4768f6f9f0d49
Bryanmankind <shegeemankind@gmail.com>
2024-04-10 13:00:53 +0100

    pizza-menu

--- src/index.js
+++ src/index.js
@@ -15,11 +15,10 @@ function App() {
 }
 
 function Header () {
-  const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
 
   return (
     <header className="header">
-      <h1 style={style}>Fast React Pizza Co.</h1>
+      <h1>Fast React Pizza Co.</h1>
     </header>
   )
 }
